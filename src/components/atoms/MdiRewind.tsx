b6eedf2b3bca8ccf4a0a66843db5339362613178
Kodai Aoyama <kodai.aoyama+github@gmail.com>
2024-05-08 20:26:38 +0900

    playerが利用しているiconifyで意図しない外部リクエストが発生していたので修正

---
+++ src/components/atoms/MdiRewind.tsx
@@ -0,0 +1,7 @@
+import type { SVGProps } from 'react';
+
+const MdiRewind = (props: SVGProps<SVGSVGElement>) => {
+    return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="m11.5 12l8.5 6V6m-9 12V6l-8.5 6z"></path></svg>);
+}
+
+export { MdiRewind }
--- src/components/molecules/AudioPlayer.tsx
+++ src/components/molecules/AudioPlayer.tsx
@@ -1,5 +1,11 @@
 import Player from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
+import { MdiPlayCircle } from '../atoms/MdiPlayCircle';
+import { MdiPauseCircle } from '../atoms/MdiPauseCircle';
+import { MdiRewind } from '../atoms/MdiRewind';
+import { MdiFastForward } from '../atoms/MdiFastForward';
+import { MdiVolumeHigh } from '../atoms/MdiVolumeHigh';
+import { MdiVolumeMute } from '../atoms/MdiVolumeMute';
 
 type AudioPlayerProps = {
     filePath: string
@@ -14,10 +20,18 @@ const AudioPlayer = (props: AudioPlayerProps): JSX.Element => {
         <div style={style} className={className} >
             <Player
                 src={filePath}
-                onPlay={e => console.log("onPlay")}
+                onPlay={() => { }}
                 style={{ background: "rgba(255,255,255,0.9)", height: "100px" }}
                 customAdditionalControls={[<></>]}
-                showSkipControls={true}
+                // bundle for offline
+                customIcons={{
+                    play: MdiPlayCircle({}),
+                    pause: MdiPauseCircle({}),
+                    rewind: MdiRewind({}),
+                    forward: MdiFastForward({}),
+                    volume: MdiVolumeHigh({}),
+                    volumeMute: MdiVolumeMute({}),
+                }}
             // other props here
             />
         </div>
