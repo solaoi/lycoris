0db2e57f21bb983a3e64e740c08611f663a87919
Kodai Aoyama <kodai.aoyama+github@gmail.com>
2024-11-08 23:24:52 +0900

    refactor: rename is_continuous flag for better clarity of termination mode

--- src-tauri/src/module/record.rs
+++ src-tauri/src/module/record.rs
@@ -24,8 +24,8 @@ use tauri::{api::path::data_dir, AppHandle, Manager};
 
 use super::{
     chat_online, recognizer::MyRecognizer, sqlite::Sqlite, transcription, transcription_amivoice,
-    transcription_ja, transcription_online, translation_en, translation_ja, translation_ja_high,
-    writer::Writer,
+    transcription_hybrid, transcription_ja, transcription_online, translation_en, translation_ja,
+    translation_ja_high, writer::Writer,
 };
 
 pub struct Record {
@@ -258,6 +258,17 @@ impl Record {
                                 if let Some(singleton) = lock.as_mut() {
                                     singleton.start(stop_convert_rx_clone, false);
                                 }
+                            } else if transcription_accuracy_clone.starts_with("hybrid-transcript")
+                            {
+                                transcription_hybrid::initialize_transcription_hybrid(
+                                    app_handle_clone,
+                                    note_id,
+                                );
+                                let mut lock =
+                                    transcription_hybrid::SINGLETON_INSTANCE.lock().unwrap();
+                                if let Some(singleton) = lock.as_mut() {
+                                    singleton.start(stop_convert_rx_clone, false);
+                                }
                             } else {
                                 transcription::initialize_transcription(
                                     app_handle_clone,
@@ -300,6 +311,7 @@ impl Record {
             translation_ja_high::drop_translation_ja_high();
             transcription_online::drop_transcription_online();
             transcription_amivoice::drop_transcription_amivoice();
+            transcription_hybrid::drop_transcription_hybrid();
             chat_online::drop_chat_online();
         } else {
             drop(stop_convert_tx)
